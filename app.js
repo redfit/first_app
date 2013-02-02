6987c9a75ae1bdc582d28785dcf15dcdb8656296
daisuke fuchiwaki <daisuke.fuchiwaki@excel.co.jp>
2013-02-02 16:26:32 +0900

    guardの設定

--- app.js
+++ app.js
@@ -1,54 +1,31 @@
 //<debug>
 Ext.Loader.setPath({
-    'Ext': 'touch/src',
-    'first_app': 'app'
+  'Ext': 'touch/src',
+  'first_app': 'app'
 });
 //</debug>
 
 Ext.application({
-    name: 'first_app',
+  name: 'first_app',
 
-    requires: [
-        'Ext.MessageBox'
-    ],
+  launch: function() {
+    Ext.create("Ext.tab.Panel", {
+      fullscreen: true,
+      tabBarPosition: 'bottom',
+      items: [
+        {
+          title: 'Home',
+          iconCls: 'home',
+          html: [
+            '<img src="http://staging.sencha.com/img/sencha.png" />',
+            '<h1>Welcome to Sencha Touch</h1>',
+            "<p>You're creating the Getting Started app. This demonstrates how ",
+            "to use tabs, lists and forms to create a simple app</p>",
+            '<h2>Sencha Touch (2.0.0)</h2>'
+          ].join("") // joinなくても綺麗に映る
+        }
+      ]
+    });
+  }
 
-    views: ['Main'],
-
-    icon: {
-        '57': 'resources/icons/Icon.png',
-        '72': 'resources/icons/Icon~ipad.png',
-        '114': 'resources/icons/Icon@2x.png',
-        '144': 'resources/icons/Icon~ipad@2x.png'
-    },
-
-    isIconPrecomposed: true,
-
-    startupImage: {
-        '320x460': 'resources/startup/320x460.jpg',
-        '640x920': 'resources/startup/640x920.png',
-        '768x1004': 'resources/startup/768x1004.png',
-        '748x1024': 'resources/startup/748x1024.png',
-        '1536x2008': 'resources/startup/1536x2008.png',
-        '1496x2048': 'resources/startup/1496x2048.png'
-    },
-
-    launch: function() {
-        // Destroy the #appLoadingIndicator element
-        Ext.fly('appLoadingIndicator').destroy();
-
-        // Initialize the main view
-        Ext.Viewport.add(Ext.create('first_app.view.Main'));
-    },
-
-    onUpdated: function() {
-        Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function(buttonId) {
-                if (buttonId === 'yes') {
-                    window.location.reload();
-                }
-            }
-        );
-    }
 });
